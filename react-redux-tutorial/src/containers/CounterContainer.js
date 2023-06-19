fb32d45b01e31eee88dff37813e225c91223d872
swhana <swhan0428@gmail.com>
2023-06-19 13:47:13 +0900

    Add Hooks
    - useSelector, useDispatch Hook 활용

--- react-redux-tutorial/src/containers/CounterContainer.js
+++ react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,9 +1,18 @@
-import { connect } from "react-redux";
+import { connect, useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
+import { useCallback } from "react";
 
-const CounterContainer = ({ number, increase, decrease }) => {
-  return <Counter number={number} onIncrease={increase} onDecrease={decrease} />;
+const CounterContainer = () => {
+  const number = useSelector(state => state.counter.number);
+  const dispatch = useDispatch();
+
+  //useCallback을 사용한 컴포넌트 성능 최적화
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch])
+
+
+  return <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />;
 }
 
 //store안의 state를 props로 넘겨주기 위해 설정하는 함수
@@ -24,12 +33,16 @@ const CounterContainer = ({ number, increase, decrease }) => {
 
 // connect()를 사용해서 Counter 컴포넌트를 redux와 연동
 // export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
-export default connect(
-  state => ({
-    number: state.counter.number,
-  }),
-  {
-    increase,
-    decrease,
-  }
-)(CounterContainer);
+// export default connect(
+//   state => ({
+//     number: state.counter.number,
+//   }),
+//   {
+//     increase,
+//     decrease,
+//   }
+// )(CounterContainer);
+
+//useSelector Hook을 사용하면 connect를 사용할 필요도 없음(store 내부 state 사용 가능)
+//useDispatch Hook을 사용하면 store 내장 함수 dispatch를 사용할 수 있음
+export default CounterContainer;
