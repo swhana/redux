c481e963d940348dff18033de32d150d0a1180c9
swhana <swhan0428@gmail.com>
2023-06-27 15:59:41 +0900

    Add Server
    - Express로 서버 코드 작성(index.server.js)
    - 5000포트로 빌드 시 에러 발생 -> 5001 포트 개방
    - SSR 적용 성공(Javascript 및 css는 적용안된 상태)

--- ssr/src/index.server.js
+++ ssr/src/index.server.js
@@ -1,8 +1,25 @@
 //SSR 전용 엔트리파일(index.js 대체)
 import ReactDOMServer from 'react-dom/server';
+import express from 'express';
+import { StaticRouter } from 'react-router-dom/server';
+import App from './App';
 
-const html = ReactDOMServer.renderToString(
-  <div>This is Server-Side Rendering</div>
-);
+const app = express();
 
-console.log(html);
+//SSR을 처리할 핸들러 함수
+const serverRender = (req, res, next) => {
+  const context = {};
+  const jsx = (
+    <StaticRouter location={req.url} context={context}>
+      <App />
+    </StaticRouter>
+  );
+  const root = ReactDOMServer.renderToString(jsx); //jsx를 string 문자열로 렌더링
+  res.send(root); //렌더링한 문자열을 클라이언트에 전송(response)
+}
+
+app.use(serverRender);
+
+app.listen(5001, () => {
+  console.log('running server on localhost');
+});
