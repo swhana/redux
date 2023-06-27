e3e3084b924077b7834e2286e2e49ac9b5836da3
swhana <swhan0428@gmail.com>
2023-06-27 16:21:22 +0900

    Add static files rendering feature
    - build에 있는 JS, CSS 정적파일에 접근해 서버에서 렌더링

--- ssr/src/index.server.js
+++ ssr/src/index.server.js
@@ -3,6 +3,30 @@ import ReactDOMServer from 'react-dom/server';
 import express from 'express';
 import { StaticRouter } from 'react-router-dom/server';
 import App from './App';
+import path from 'path';
+import fs from 'fs';
+
+const manifest = JSON.parse(fs.readFileSync(path.resolve('./build/asset-manifest.json'), 'utf8'));
+
+function createPage(root) {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="shortcut icon" href="/favicon.ico"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no"/>
+    <meta name="theme-color" content="#000000"/>
+    <title>React App</title>
+    <link href="${manifest.files['main.css']}" rel="stylesheet"/>
+  </head>
+  <body>
+    <div id="root">${root}</div>
+    <script src="${manifest.files['main.js']}"></script>
+  </body>
+  </html>
+  `
+}
 
 const app = express();
 
@@ -15,9 +39,14 @@ const serverRender = (req, res, next) => {
     </StaticRouter>
   );
   const root = ReactDOMServer.renderToString(jsx); //jsx를 string 문자열로 렌더링
-  res.send(root); //렌더링한 문자열을 클라이언트에 전송(response)
+  res.send(createPage(root)); //렌더링한 문자열을 클라이언트에 전송(response)
 }
 
+const serve = express.static(path.resolve('./build'), {
+  index: false, // '/' 경로에서 index.html을 보여주지 않도록 설정
+})
+
+app.use(serve); //반드시 serverRender 전에 serve가 위치해야함
 app.use(serverRender);
 
 app.listen(5001, () => {
